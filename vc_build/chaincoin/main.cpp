7d07acf9d623464fbb8a7d1a20fa78fec1d291e8
c z <5069099@qq.com>
2022-09-22 21:38:40 +0800

    提交修改

--- vc_build/chaincoin/main.cpp
+++ vc_build/chaincoin/main.cpp
@@ -1,11 +1,19 @@
-#include "../../src/uint256.h"
-#include "../../src/primitives/block.h"
+
+#include <uint256.h>
+#include <hash.h>
+#include <primitives/block.h>
 
 
 
 int main()
 {
-	CBlock b;
+	CBlockHeader b;
+
+	std::string s = "03ba3680749a1e1100d13648c20cfc7838a9ee637ead996615f6bbe3e324ac28e2";
+	uint256 u256 = Hash(s);
+	uint160 u160 = Hash160(u256);
+
+
 
 	return 0;
 }
