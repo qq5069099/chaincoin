7530dd9bd89bc715c97f629bd3b06cbf587e3a7b
c z <5069099@qq.com>
2022-09-23 00:43:10 +0800

    测试地址生成

--- vc_build/chaincoin/main.cpp
+++ vc_build/chaincoin/main.cpp
@@ -2,6 +2,8 @@
 #include <uint256.h>
 #include <hash.h>
 #include <primitives/block.h>
+#include <base58.h>
+#include <util/strencodings.h>
 
 
 
@@ -9,11 +11,25 @@ int main()
 {
 	CBlockHeader b;
 
-	std::string s = "03ba3680749a1e1100d13648c20cfc7838a9ee637ead996615f6bbe3e324ac28e2";
-	uint256 u256 = Hash(s);
-	uint160 u160 = Hash160(u256);
+	std::string s = "03dd9859a23571a513d811925669a53d6f3475c197ace390218e0106f497a59fa4";
+	auto v = ParseHex(s);
 
+	uint160 u160 = Hash160(v);
+	std::string u160_str = u160.GetHex();
+	
+	std::vector<unsigned char> vv(u160.begin(), u160.end());
+	vv.insert(vv.begin(), 0);
 
+	std::string addr = EncodeBase58Check(vv);
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
