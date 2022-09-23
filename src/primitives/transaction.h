1e9ba7990c345fd92de05c4ad20818da1a302f8d
c z <5069099@qq.com>
2022-09-23 17:19:11 +0800

    modify

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -14,6 +14,7 @@ class CTransaction
     uint256         scriptSig;          // 签名字符串
     CAccountAddr    toAddr;             // 锁定脚本
     CAmount         nValue;             // 输出金额
+    CAmount         nFree;              // 输出金额
 
     uint32_t        nLockTime;          // 锁定时间
 };
