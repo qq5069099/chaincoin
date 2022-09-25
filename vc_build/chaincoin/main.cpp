490108d17c4e2a0fbba86d08388c704f7fbd394e
qq5069099 <5069099@qq.com>
2022-09-26 03:08:43 +0800

    测试UniValue库与加密库

--- vc_build/chaincoin/main.cpp
+++ vc_build/chaincoin/main.cpp
@@ -4,6 +4,8 @@
 #include <primitives/block.h>
 #include <base58.h>
 #include <util/strencodings.h>
+#include <univalue.h>
+#include <secp256k1.h>
 
 
 
@@ -23,9 +25,20 @@ int main()
 	std::string addr = EncodeBase58Check(vv);
 
 	//测试修改
+	UniValue uniValue(UniValue::VARR);
+	uniValue.push_backV(vv.begin(), vv.end());
+	std::string ss = uniValue.write();
 
-	//222
+	secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
+	secp256k1_pubkey pubkey;
+	int ret = secp256k1_ec_pubkey_create(ctx, &pubkey, (const unsigned char*)"54645545454654654645644889875556445546");
 
+	unsigned char vch[65];
+	size_t clen = 65;
+	bool fCompressed = false;
+	secp256k1_ec_pubkey_serialize(ctx, (unsigned char*)vch, &clen, &pubkey, fCompressed ? SECP256K1_EC_COMPRESSED : SECP256K1_EC_UNCOMPRESSED);
+
+	secp256k1_context_destroy(ctx);
 	return 0;
 }
 
